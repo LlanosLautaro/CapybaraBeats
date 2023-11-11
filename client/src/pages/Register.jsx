a01c631e0c7fbca212b1c26fecb2084b8e86b99b
Lautaro <lautarollanosnahuel@gmail.com>
2023-11-10 23:29:04 -0300

    Create Fetch hook

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,63 +1,17 @@
-import React, { useState } from 'react';
+import { useFetch } from "../useFetch";
 
-export function Register() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [username, setUsername] = useState('');
+export function Register(){
+  const {data} = useFetch("http://127.0.0.1:8000/music/list/")
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const data = {
-      email,
-      password,
-      username,
-    };
-
-    try {
-      const response = await fetch('http://127.0.0.1:8000/users/create/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      });
-
-      if (response.ok) {
-        // La solicitud fue exitosa, puedes manejar la respuesta aquí.
-      } else {
-        // Maneja cualquier error aquí, por ejemplo, mostrando un mensaje al usuario.
+  
+  return(
+    <div classname="xd">
+      {
+        data?.map((song)=> (
+          <li key = {song.id}> {song.title}    </li>
+        )
+        )
       }
-    } catch (error) {
-      console.error('Error al realizar la solicitud:', error);
-    }
-  };
-
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Contraseña"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Nombre de usuario"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <button type="submit">Registrarse</button>
-      </form>
     </div>
   );
-}
-
-export default Register;
+};
