15b2035ae029389f9ffe9ee11088c58eeec0c45c
Lautaro <lautarollanosnahuel@gmail.com>
2023-11-14 15:01:42 -0300

    create base register

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,17 +1,79 @@
-import { useFetch } from "../useFetch";
+import React, { useState } from 'react';
 
-export function Register(){
-  const {data} = useFetch("http://127.0.0.1:8000/music/list/")
+export function Register() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
 
-  
-  return(
-    <div classname="xd">
-      {
-        data?.map((song)=> (
-          <li key = {song.id}> {song.title}    </li>
-        )
-        )
-      }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const datos = {
+      email: email,
+      password: password,
+      username: username
+    };
+
+    const opciones = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(datos)
+    };
+
+    const url = 'http://127.0.0.1:8000/users/create/';
+
+    fetch(url, opciones)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Error en la solicitud');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('Registro exitoso:', data);
+        //aca hay que poner lo que pasa despues de hacer el registro
+      })
+      .catch(error => {
+        console.error('Error en el registro:', error);
+        // Manejar errores del registro aquí
+      });
+  };
+
+  return (
+    <div>
+      <h2>Registro de Usuario</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Contraseña:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Nombre de Usuario:</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Registrarse</button>
+      </form>
     </div>
   );
-};
+}
